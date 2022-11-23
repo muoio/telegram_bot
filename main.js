25bc25f7155cd63343fdf404feb25b5d4d0cdc51
muoio <tuana2tqt@gmail.com>
2022-11-23 19:15:22 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+const { resolve } = require('path');
 const path = require('path')
+const puppeteer = require('puppeteer');
+
+let browser, page, Authorizations;
+
+async function init(){
+  browser = await puppeteer.launch({ headless: false,defaultViewport: null });
+  page = await browser.newPage();
+}
+async function login(){
+  await page.goto('https://fwt.fialda.com/phaisinh/VN30F1M/tong-quan' ,{ waitUntil: 'networkidle0' });
+  return new Promise(resolve =>{
+    let login_process = setInterval(async () => {
+      let cookies = await page.cookies('https://fwt.fialda.com');
+      for(let cookie of cookies)
+        if(cookie.name == 'Abp.LongAuthToken')
+          if (cookie.value.length > 0){
+            // cookies = get_nessecery_cookies(cookies);
+            // save_new_user(cookie.value, cookies);
+            Authorizations = cookie.value;
+            clearInterval(login_process);
+            //browser.close();
+            console.log("LOGGINED")
+            resolve(true);
+            return true;
+          }
+    },5000);
+  })
+}
+
+async function phaisinh(){
+  console.log("PHAISINH")
+  return await page.evaluate(async () => {
+    let raw = "[{\"symbol\":\"VN30F2212\",\"changeRecords\":{\"BasicInfo\":0,\"BasicPrice\":0,\"Price\":0,\"BidAsk\":0,\"PTInfo\":0,\"ForeignTrade\":0,\"RealtimeStatistic\":0}},{\"symbol\":\"VN30F2301\",\"changeRecords\":{\"BasicInfo\":0,\"BasicPrice\":0,\"Price\":0,\"BidAsk\":0,\"PTInfo\":0,\"ForeignTrade\":0,\"RealtimeStatistic\":0}},{\"symbol\":\"VN30F2303\",\"changeRecords\":{\"BasicInfo\":0,\"BasicPrice\":0,\"Price\":0,\"BidAsk\":0,\"PTInfo\":0,\"ForeignTrade\":0,\"RealtimeStatistic\":0}},{\"symbol\":\"VN30F2306\",\"changeRecords\":{\"BasicInfo\":0,\"BasicPrice\":0,\"Price\":0,\"BidAsk\":0,\"PTInfo\":0,\"ForeignTrade\":0,\"RealtimeStatistic\":0}}]";
+    let requestOptions = {
+      method: 'POST',
+      headers: {"Content-Type": "application/json;charset=UTF-8"},
+      body: raw,
+      redirect: 'follow'
+    };
+    return fetch("https://fwtapi4.fialda.com/api/services/app/Common/GetDerInfos", requestOptions)
+      .then(response => response.text())
+      .then(text => JSON.parse(text))
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  });
+}
 
 function createWindow () {
   // Create the browser window.
@@ -22,7 +69,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   createWindow()
 
   app.on('activate', function () {
@@ -30,17 +77,23 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+  await init();
+  await login();
+  await phaisinh();
+
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+
+
+
 const { Telegraf } = require("telegraf");
 
 const YOUR_TOKEN = "5603880218:AAGRMoQel50e2KxXV6r_cSrfeYuBMejNe38";
@@ -50,15 +103,6 @@ bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 
 bot.on("sticker", (ctx) => ctx.reply("🐶"));
-
-// bot.on("message", async (ctx) => {
-//   const message = ctx.update.message.text;
-//   if (message.match(/hello/)) {
-//     ctx.reply("Xin chào");
-//   } else {
-//     ctx.reply("Hong hiểu...");
-//   }
-// });
 
 bot.command('command', async (ctx) => {
   return await ctx.reply('this is text', Markup
@@ -72,7 +116,10 @@ bot.command('command', async (ctx) => {
   )
 })
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'));
+bot.command('ps', (ctx) => {
+  console.log(ctx.message.text);
+  ctx.reply('Hello') 
+});
 
 
 bot.launch();
